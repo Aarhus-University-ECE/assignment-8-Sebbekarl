969c2cfd1d5b3bd55b74e9958d39d0b3361a737b
sebbekarl <sebbekarl97@gmail.com>
2022-10-07 16:04:08 +0200

    temp

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -14,7 +14,7 @@ void sort(linked_list *llPtr)
     // repeat through the entire array
 
     node_t *curr_node = llPtr->head->next;
-    node_t *insert_front, *insert_back, *old_back, *old_front, *test_back, *test_front;
+    node_t *insert_front, *insert_back, *old_back, *old_front;
     node_t *previous_node = llPtr->head;
 
     while (curr_node != NULL)
@@ -22,41 +22,29 @@ void sort(linked_list *llPtr)
         if (curr_node->data < previous_node->data)
         {
             node_t *node_temp = llPtr->head;
-            //printf("%d",curr_node->data);
-            
-            
-            old_back = previous_node;
-            old_front = curr_node->next;
-            old_back->next = old_front;
+
+            old_back = previous_node; //head
+            old_front = curr_node->next; //head->next->next
+            old_back->next = old_front;  //head->next = head->next->next
+
             while (node_temp != curr_node)
             {
-                
-                if(node_temp->data > curr_node->data)
+
+                if (node_temp->data > curr_node->data) //head > head->next
                 {
-                    
-                    insert_front = node_temp;
-                    insert_back = node_temp->next;
+
+                    insert_front = node_temp; //head
+                    insert_back = node_temp->next; //head->next->next
                     break;
-                    //test_back = insert_back;
-                    //test_front = insert_front;
                 }
                 node_temp = node_temp->next; 
             }
-            
 
-            printf("%d, %d\n",insert_front->data, insert_back->data,llPtr->head);
-            insert_front->next = curr_node;
+            printf("%d, %d\n", insert_front->data, insert_back->data, llPtr->head);
+            insert_front->next = curr_node; //head->next 
             curr_node->next = insert_back;
-            /*
-            if(previous_node == llPtr->head)
-            {
-                curr_node->next = llPtr->head->next;
-                llPtr->head = curr_node;
-            }
-            */
-
         }
-        
+
         Sleep(1);
         previous_node = curr_node;
         curr_node = curr_node->next;
