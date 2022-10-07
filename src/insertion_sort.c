f916e2eb77cf96f5c7df6ba9ac50b91bb9a407e6
sebbekarl <sebbekarl97@gmail.com>
2022-10-07 16:13:38 +0200

    test

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -21,28 +21,8 @@ void sort(linked_list *llPtr)
     {
         if (curr_node->data < previous_node->data)
         {
-            node_t *node_temp = llPtr->head;
+            
 
-            old_back = previous_node; //head
-            old_front = curr_node->next; //head->next->next
-            old_back->next = old_front;  //head->next = head->next->next
-
-            while (node_temp != curr_node)
-            {
-
-                if (node_temp->data > curr_node->data) //head > head->next
-                {
-
-                    insert_front = node_temp; //head
-                    insert_back = node_temp->next; //head->next->next
-                    break;
-                }
-                node_temp = node_temp->next; 
-            }
-
-            printf("%d, %d\n", insert_front->data, insert_back->data, llPtr->head);
-            insert_front->next = curr_node; //head->next 
-            curr_node->next = insert_back;
         }
 
         Sleep(1);
